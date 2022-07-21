4620894b6a861d01bc678d0c64ae4860e38a304d
Dāvis R <dafels94@gmail.com>
2022-07-21 21:15:05 +0300

    Rename the API project

--- Horse.Business/Enums.cs
+++ Horse.Business/Enums.cs
@@ -1,19 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Horse.Business
+﻿namespace Horse.Business
 {
     public enum WeightUnit
     {
         Kg = 0,
         Lb = 1
-    }
-
-    public enum WeightTarget
-    {
-
     }
 }
